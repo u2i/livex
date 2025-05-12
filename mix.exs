b85fe04ed99029963917ce85395583db175e3872
Tom Clarke <tom@u2i.com>
2025-05-13 00:46:24 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Livex.MixProject do
   def project do
     [
       app: :livex,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
