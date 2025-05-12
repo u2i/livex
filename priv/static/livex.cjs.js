5210b282f7be42dae111fb3b646b7c3e66b12e97
Tom Clarke <tom@u2i.com>
2025-05-12 12:56:31 +0200

    Updated assets

--- priv/static/livex.cjs.js
+++ priv/static/livex.cjs.js
@@ -107,10 +107,6 @@ function enhanceLiveSocket(liveSocket) {
       const uris = buildLvPageUrls();
       const url1 = new URL(uris.primaryUrl, window.location.origin);
       const url = new URL(uris.combinedUrl, window.location.origin);
-      console.log("updating view href");
-      console.log("before " + window.liveSocket.href);
-      console.log("after " + url1.href);
-      console.log("after 2" + url.href);
       liveSocket.historyPatch(url1.href, "push");
       window.liveSocket.href = url1.href;
       if (window.liveSocket.main) {
@@ -124,8 +120,6 @@ function enhanceLiveSocket(liveSocket) {
     liveSocket.historyPatch(href, linkState);
   };
   window.addEventListener("phx:js-execute", ({ detail }) => {
-    console.log("js-execute");
-    console.log(detail.ops);
     liveSocket.execJS(document.body, detail.ops);
   });
   return liveSocket;
